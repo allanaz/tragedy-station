eed4770d0c03f6d051bbe8ff1878dcbb41a785a7
Allan Garcia-Zych <allangz@mit.edu>
2019-06-28 15:55:56 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import TimeCalculator from './timeCalculator.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Fishery No. 1</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome Fisherperson.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TimeCalculator />
       </header>
     </div>
   );
---
+++ src/timeCalculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+class TimeCalculator extends React.Component {
+  render() {
+    return (
+      <h3>Enter an Amount of fish to harvest</h3>
+    );
+  }
+}
+
+export default TimeCalculator;
