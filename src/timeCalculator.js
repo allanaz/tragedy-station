d1a0db1d13af05b7afe2352d192d699cc1d4f236
gu-yun <gu.yun54@gmail.com>
2019-07-01 13:19:39 -0400

    timeCalculator stuff

--- src/timeCalculator.js
+++ src/timeCalculator.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import './App.css';
-import { Button, Icon } from 'semantic-ui-react';
+import { Button, Form, Modal } from 'semantic-ui-react';
 
 class TimeCalculator extends React.Component {
   constructor(props) {
     super(props)
 
+    this.handleChange = this.handleChange.bind(this)
     this.calculateTime = this.calculateTime.bind(this)
+    this.state = {
+      numFishRequested: null,
+      fishingTime: 0
+    }
   }
 
+  handleChange = (e) => {
+    this.setState({numFishRequested: e.target.value})
+  }
 
-  calculateTime = () => {
-    
+  // have amount left be passed down as a prop
+  calculateTime = (fishLeft, numFishRequested) => {
+    this.setState({fishingTime: 10})
   }
 
   render() {
     return (
       <div>
-        <h3>Enter an amount of fish to harvest</h3>
-        <Button 
-          content="fish!" />
+        <Form>
+          <Form.Field>
+            <label>Enter an amount to fish:</label>
+            <input 
+              placeholder='integers only please'
+              value={this.state.numFishRequested}
+              onChange={this.handleChange}/>
+          </Form.Field>
+          <Modal trigger={<Button onClick={this.calculateTime}>Fish!</Button>}>
+            <Modal.Content>
+            {/* <Countdown fishingTime={this.fishingTime}/> */}
+            </Modal.Content>
+          </Modal>
+        </Form>
       </div>
     );
   }
