5ef0b2b7abf7331e271fd863f233782472bc177e
gu-yun <gu.yun54@gmail.com>
2019-07-01 11:45:52 -0400

    created station container, constructor for timeCalculator

--- src/timeCalculator.js
+++ src/timeCalculator.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Button, Icon } from 'semantic-ui-react';
 
 class TimeCalculator extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.calculateTime = this.calculateTime.bind(this)
+  }
+
+
+  calculateTime = () => {
+    
+  }
+
   render() {
     return (
-      <h3>Enter an Amount of fish to harvest</h3>
+      <div>
+        <h3>Enter an amount of fish to harvest</h3>
+        <Button 
+          content="fish!" />
+      </div>
     );
   }
 }
